0f7526dd120a7c421f7dd63c71257e18036f9286
AlexeyVlasov <sketchrsix@gmail.com>
2022-07-05 01:10:29 +0300

    logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
 
-import Logo from "../utils/tiktik-logo.png";
+import Logo from "../utils/vlasov-logo.png";
 
 const Navbar = () => {
 	return (
